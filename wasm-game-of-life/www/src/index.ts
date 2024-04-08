4739c42435803842bb3148c23dbc3e6efcd74e40
berlinen <1292393108@qq.com>
2024-04-08 17:59:36 +0800

    feat: play and pause

--- wasm-game-of-life/www/src/index.ts
+++ wasm-game-of-life/www/src/index.ts
@@ -4,6 +4,8 @@ const pre = document.getElementById('game-of-life-canvas');
 
 const universe = Universe.new();
 
+let animationId = null;
+
 const renderLoop = () => {
   // @ts-ignore
   pre.textContent = universe.render();
@@ -14,3 +16,31 @@ const renderLoop = () => {
 };
 
 requestAnimationFrame(renderLoop);
+
+const isPasued = () => {
+  return animationId === null;
+};
+
+const playPauseButton = document.getElementById('play-pause');
+
+const play = () => {
+  // @ts-ignore
+  playPauseButton.textContent = '⏸';
+  renderLoop();
+};
+
+const pause = () => {
+  // @ts-ignore
+  playPauseButton.textContent = '▶';
+  cancelAnimationFrame(animationId!);
+  animationId = null;
+};
+
+// @ts-ignore
+playPauseButton.addEventListener('click', () => {
+  if (isPasued()) {
+    play();
+  } else {
+    pause();
+  }
+});
